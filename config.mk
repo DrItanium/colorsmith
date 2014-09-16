41e1a38e62d43ab54be9cf1694dfff3bb922b875
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-09-15 22:03:54 -0700

    Eliminated the flow header

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 CC = cc
 OUTPUT = colorsmith 
-PREFIX = /usr/local
+PREFIX = ${HOME}
 LDFLAGS = -lm -lrt -lwiringPi -L/usr/local/lib -lwiringPiDev
 CFLAGS = -O2 -I/usr/local/include 
 
--- flow.c
+++ flow.c
@@ -4,8 +4,25 @@
 #include <piGlow.h>
 #include <string.h>
 #include <errno.h>
-#include "flow.h"
 
+/* Instruction particle */
+typedef unsigned char byte;
+typedef unsigned short ushort;
+enum {
+   LEDCount = 18,
+};
+
+typedef struct FlowContainer {
+   byte cells[LEDCount];
+} FlowContainer;
+
+
+/* inefficient but who cares */
+void setup(FlowContainer* container);
+void shutdown(FlowContainer* container);
+void decode(FlowContainer* container, FILE* input);
+void shiftcells(FlowContainer* container, byte value);
+void updateglow(FlowContainer* container);
 
 byte legarray [] = {
    0, 0, 0, 0, 0, 0,
