c92cbeed9ece54ad64691676b52b434a104b1c86
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-01-04 19:32:29 -0800

    Added the stupid simple program I used to derive the led -> piGlow map

---
+++ base6table.c
@@ -0,0 +1,24 @@
+/* Used to figure out that we are dealing with base 6 mathematics to interface
+ * with the wiringPi libs with respect to the piGlow
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include "types.h"
+
+int main() {
+   int i;
+   printf("Legend\n");
+   printf("--------------\n");
+   printf("- x: legarray\n");
+   printf("- y: ringarray\n");
+   printf("- z: led index\n");
+   printf("--------------\n");
+   printf("Format is: z <=> (x,y)\n");
+   printf("\n\n");
+   for (i = 0; i < 18; i++) {
+      printf("%d <=> (%d, %d)\n", i, legarray[i], ringarray[i]);
+   }
+   return 0;
+}
