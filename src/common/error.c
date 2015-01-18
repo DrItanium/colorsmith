f800f176ffb8537d9a3ee4321f4b08b31fb4f64f
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-01-17 18:50:25 -0800

    Split custom_error off into a separate file for linking purposes

--- src/common/error.c
+++ src/common/error.c
@@ -7,11 +7,3 @@ void error(const char* message, int code) {
    fprintf(stderr, "%s\n", message);
    exit(code);
 }
-
-void custom_error(int code, const char* fmt, ...) {
-	va_list args;
-	va_start(args, fmt);
-	vfprintf(stderr, fmt, args);
-	va_end(args);
-	exit(code);	
-}
