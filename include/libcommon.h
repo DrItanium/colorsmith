155e1b62e0a61d1d7277a0767b9cd4dcb538e0f6
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-01-17 16:13:53 -0800

    diverged all of the headers into separate entries

---
+++ include/libcommon.h
@@ -0,0 +1,6 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+void error(const char* message, int code);
+
+#endif 
--- include/types.h
+++ include/types.h
@@ -1,25 +1,7 @@
 #ifndef _TYPES_H
 #define _TYPES_H
-/* Instruction particle */
-enum {
-	LEDCount = 18,
-	FieldCount = LEDCount + 1,
-};
 
 typedef unsigned char byte;
 typedef unsigned short ushort;
-
-void error(const char* message, int code);
-
-byte legarray [] = {
-   0, 0, 0, 0, 0, 0,
-   1, 1, 1, 1, 1, 1,
-   2, 2, 2, 2, 2, 2,
-};
-byte ringarray [] = {
-   0, 1, 2, 3, 4, 5,
-   0, 1, 2, 3, 4, 5,
-   0, 1, 2, 3, 4, 5,
-};
 
 #endif 
